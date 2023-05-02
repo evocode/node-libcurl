5e9e5370d56998e2d550e8ade8a6ff2cbe09fbac
Patrick Heeney <patrickheeney@gmail.com>
2023-05-02 16:03:05 -0700

    Build was including wrong curl

--- scripts/ci/build-libcurl-impersonate.sh
+++ scripts/ci/build-libcurl-impersonate.sh
@@ -16,36 +16,39 @@ mkdir -p $2/source
 FORCE_REBUILD=${FORCE_REBUILD:-}
 FORCE_REBUILD_LIBCURL=${FORCE_REBUILD_LIBCURL:-}
 
+# https://github.com/evocode/curl-impersonate/blob/main/Makefile.in#LL19C17-L19C28
+CURL_VERSION="7.84.0"
+
 # @TODO We are explicitly checking the static lib
 if [[ -f $build_folder/lib/libcurl-impersonate-chrome.a ]] && [[ -z $FORCE_REBUILD || $FORCE_REBUILD != "true" ]] && [[ -z $FORCE_REBUILD_LIBCURL || $FORCE_REBUILD_LIBCURL != "true" ]]; then
   echo "Skipping rebuild of libcurl-impersonate because lib file already exists"
   exit 0
 fi
 
-echo "Preparing release for libcurl"
+# echo "Preparing release for libcurl"
 
-CURL_VERSION=$3
-version_with_dashes=$(echo $CURL_VERSION | sed 's/\./_/g')
+# CURL_VERSION=$3
+# version_with_dashes=$(echo $CURL_VERSION | sed 's/\./_/g')
 
-if [ ! -d $2/source/libcurl ]; then
-  echo "Using release tarball"
+# if [ ! -d $2/source/libcurl ]; then
+#   echo "Using release tarball"
 
-  cd $curr_pwd
-  echo "https://github.com/curl/curl/releases/download/curl-$version_with_dashes/curl-$CURL_VERSION.tar.gz"
-  $curr_dirname/download-and-unpack.sh \
-    https://github.com/curl/curl/releases/download/curl-$version_with_dashes/curl-$CURL_VERSION.tar.gz \
-    $2
+#   cd $curr_pwd
+#   echo "https://github.com/curl/curl/releases/download/curl-$version_with_dashes/curl-$CURL_VERSION.tar.gz"
+#   $curr_dirname/download-and-unpack.sh \
+#     https://github.com/curl/curl/releases/download/curl-$version_with_dashes/curl-$CURL_VERSION.tar.gz \
+#     $2
 
-  mv $2/curl-$CURL_VERSION $2/source/libcurl
-  cd $2/source/libcurl
-else
-  cd $2/source/libcurl
-  if [ -f ./configure ]; then
-    make distclean || true;
-  else
-    ./buildconf
-  fi
-fi
+#   mv $2/curl-$CURL_VERSION $2/source/libcurl
+#   cd $2/source/libcurl
+# else
+#   cd $2/source/libcurl
+#   if [ -f ./configure ]; then
+#     make distclean || true;
+#   else
+#     ./buildconf
+#   fi
+# fi
 
 echo "Preparing release for libcurl-impersonate"
 
@@ -78,12 +81,17 @@ else
   make chrome-build && make chrome-install
 fi
 
+pwd
+ls -al
+ls -al "curl-$CURL_VERSION"
+
 cd $build_folder
 pwd
 ls -al
 
 # copy curl include to build folder
-cp -r $2/source/libcurl/include $build_folder/include
+# cp -r $2/source/libcurl/include $build_folder/include
+cp -r $2/source/$1/curl-$CURL_VERSION/include $build_folder/include
 
 # create symlinks without chrome for default usage
 cd $build_folder/bin
