d4add8da4a9c86ef9a4f30b6d712b29941cadd1d
Patrick Heeney <patrickheeney@gmail.com>
2023-05-02 16:30:29 -0700

    Add libcurl to matrix and cleanup

--- scripts/ci/build-libcurl-impersonate.sh
+++ scripts/ci/build-libcurl-impersonate.sh
@@ -13,42 +13,15 @@ curr_pwd=$(pwd)
 mkdir -p $build_folder
 mkdir -p $2/source
 
+BUNDLED_LIBCURL_VERSION=$3
 FORCE_REBUILD=${FORCE_REBUILD:-}
 FORCE_REBUILD_LIBCURL=${FORCE_REBUILD_LIBCURL:-}
-
-# https://github.com/evocode/curl-impersonate/blob/main/Makefile.in#LL19C17-L19C28
-CURL_VERSION="7.84.0"
 
 # @TODO We are explicitly checking the static lib
 if [[ -f $build_folder/lib/libcurl-impersonate-chrome.a ]] && [[ -z $FORCE_REBUILD || $FORCE_REBUILD != "true" ]] && [[ -z $FORCE_REBUILD_LIBCURL || $FORCE_REBUILD_LIBCURL != "true" ]]; then
   echo "Skipping rebuild of libcurl-impersonate because lib file already exists"
   exit 0
 fi
-
-# echo "Preparing release for libcurl"
-
-# CURL_VERSION=$3
-# version_with_dashes=$(echo $CURL_VERSION | sed 's/\./_/g')
-
-# if [ ! -d $2/source/libcurl ]; then
-#   echo "Using release tarball"
-
-#   cd $curr_pwd
-#   echo "https://github.com/curl/curl/releases/download/curl-$version_with_dashes/curl-$CURL_VERSION.tar.gz"
-#   $curr_dirname/download-and-unpack.sh \
-#     https://github.com/curl/curl/releases/download/curl-$version_with_dashes/curl-$CURL_VERSION.tar.gz \
-#     $2
-
-#   mv $2/curl-$CURL_VERSION $2/source/libcurl
-#   cd $2/source/libcurl
-# else
-#   cd $2/source/libcurl
-#   if [ -f ./configure ]; then
-#     make distclean || true;
-#   else
-#     ./buildconf
-#   fi
-# fi
 
 echo "Preparing release for libcurl-impersonate"
 
@@ -68,8 +41,8 @@ else
   make distclean || true;
 fi
 
-touch $2/source/$1/config.log
-chmod +x $2/source/$1/configure
+touch config.log
+chmod +x configure
 
 ./configure \
     --prefix=$build_folder \
@@ -81,17 +54,9 @@ else
   make chrome-build && make chrome-install
 fi
 
-pwd
-ls -al
-ls -al "curl-$CURL_VERSION"
-
-cd $build_folder
-pwd
-ls -al
-
-# copy curl include to build folder
-# cp -r $2/source/libcurl/include $build_folder/include
-cp -r $2/source/$1/curl-$CURL_VERSION/include $build_folder/include
+# copy patched libcurl include to build folder
+# curl-$BUNDLED_LIBCURL_VERSION is downloaded during previous make
+cp -r curl-$BUNDLED_LIBCURL_VERSION/include $build_folder/include
 
 # create symlinks without chrome for default usage
 cd $build_folder/bin
